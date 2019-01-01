0231e5de89959263b2451f3036f47fc6146fdb6d
Nitish Puri <npuri1903@gmail.com>
2019-01-01 16:32:18 +0530

    Single page application.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent'
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
@@ -23,9 +24,17 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
-          leader={this.state.leaders.filter((leader)=> leader.featured)[0]}/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+      );
+    };
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+        />
       );
     };
     return (
@@ -34,6 +43,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
